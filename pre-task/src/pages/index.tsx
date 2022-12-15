fc8d6d76c35412b2cf6f4c30676aac8a68a0855d
NaveOWO <pianowell96@gmail.com>
2022-12-15 15:29:31 +0900

    init project

---
+++ pre-task/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export default function Home() {
+  return <Web> 웹들아 가보자!!!!!!!!!!!!!!!!!!!!!!!!!!! </Web>;
+}
+
+const Web = styled.h1`
+  font-size: 5rem;
+  text-align: center;
+
+  margin-top: 2rem;
+`;
---
+++ pre-task/src/styles/globalStyles.ts
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from "styled-components";
+import { reset } from "styled-reset";
+
+const GlobalStyles = createGlobalStyle` 
+  ${reset}
+  * {
+    box-sizing: border-box;
+    font-family: 'Noto Sans KR' !important;
+    font-style: normal;
+  }
+  html, body {
+    width: 100%;
+    height: 100%;
+    font-size: 62.5%;
+  }
+  body > div {
+    height: 100%;
+  }
+  button:hover {
+    cursor: pointer
+  }
+
+`;
+
+export default GlobalStyles;
