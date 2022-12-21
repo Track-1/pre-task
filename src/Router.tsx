a6069d71fccad3322c0cb261b18c667d5ff3c442
Seojisoo20191941 <amy0480@naver.com>
2022-12-22 01:05:14 +0900

    [ feat ] jsPlyer 라우팅 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,9 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import JsPlayer from './pages/jsPlayer';
 
 export default function Router() {
   return (
     <BrowserRouter>
-      <Routes></Routes>
+      <Routes>
+        <Route path="/jsPlayer" element={<JsPlayer/>}></Route>
+      </Routes>
     </BrowserRouter>
   );
 }
--- src/pages/jsPlayer.tsx
+++ src/pages/jsPlayer.tsx
@@ -1,3 +1,5 @@
 export default function jsPlayer() {
-  return;
+  return(
+    <>js</>
+  );
 }
