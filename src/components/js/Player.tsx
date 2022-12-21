7944932842f8776ef0ce6d8c466ae16551050d0e
Seojisoo20191941 <amy0480@naver.com>
2022-12-22 03:37:55 +0900

    [ feat ] 오디오 삽입

--- src/components/js/Player.tsx
+++ src/components/js/Player.tsx
@@ -1,21 +1,28 @@
 import { useState } from "react";
-
+import jsMusic from "../../assets/christmas.mp3";
 
 export default function Player() {
-    const [start, setStart] = useState<boolean>(false)
+    const [play, setPlay] = useState<boolean>(false)
 
-    const handleStartButtonClick=()=>{
-        setStart((prev)=>!prev)
-        console.log(start)
+    const audio = new Audio(jsMusic)
+    
+    const handlePlayButtonClick=()=>{
+        setPlay((prev)=>!prev)
+        console.log(play)
+        audio.play()
     }
+
+
 
     return(
     <>      
-        <button type="button" onClick={()=>handleStartButtonClick()}>{!start?("▶️"):("⏸")}</button>
+        <button type="button" onClick={()=>handlePlayButtonClick()}>{!play?("▶️"):("⏸")}</button>
         <button type="button">⏹</button>
         <button type="button">⏮</button>
         <button type="button">⏭</button>
 
+        <audio src="../../assets/jsMusic.mp3"></audio>
+        
         
     </>
     );
