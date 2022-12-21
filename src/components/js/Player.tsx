8c56ca31f11ae61b1b4cb3ee8a2c40fac1b4c3bc
Seojisoo20191941 <amy0480@naver.com>
2022-12-22 01:35:52 +0900

    [ feat ] 재생 버튼들 구현

--- src/components/js/Player.tsx
+++ src/components/js/Player.tsx
@@ -1,7 +1,19 @@
+import { useState } from "react";
+
+
 export default function Player() {
+    const [start, setStart] = useState<boolean>(false)
+
+    const handleStartButtonClick=()=>{
+        setStart((prev)=>!prev)
+        console.log(start)
+    }
+
     return(<>      
-        <button type="button">start</button>
-        <button type="button">◼︎</button>
+        <button type="button" onClick={()=>handleStartButtonClick()}>{!start?("▶️"):("⏸")}</button>
+        <button type="button">⏹</button>
+        <button type="button">⏮</button>
+        <button type="button">⏭</button>
     </>
     );
 }
